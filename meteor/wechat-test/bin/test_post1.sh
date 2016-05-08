8075b6f649e3317fa27c9ba443dbb50bef654c42
zhaoic <zhaoic@163.com>
2016-05-08 23:28:04 +0800

    add curl POST with json

--- meteor/wechat-test/bin/test_post1.sh
+++ meteor/wechat-test/bin/test_post1.sh
@@ -1,1 +1,2 @@
-curl -d {"action_name": "QR_LIMIT_SCENE","action_info":{"scene":{ "scene_id": scene_id}}} http://localhost:3000/echo
+#success post data and get ticket
+curl -d '{"action_name": "QR_LIMIT_SCENE","action_info":{"scene":{ "scene_id": 3456}}}' https://api.weixin.qq.com/cgi-bin/qrcode/create?access_token=Itcl3Ygv7FuiUmWDPhJj6OBPsrcrG093itMgt8BADa66pJRuEMcEKeYN055OYsxgCrKkoSUviXmYdNoqF0jjFgY_nq8RHxZEyhJ9r4SW3jU3QBC6ILRiAIHqy_geuAA4JWRhABAFSN
