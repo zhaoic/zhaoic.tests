b2748926de9c6456f6be51de274e4b01dd1a0aab
zhaoic <zhaoic@163.com>
2016-06-28 22:13:45 +0800

    add angular example

---
+++ meteor/meteor-tutorial/simple-todos-angular/imports/components/todosList/todosList.js
@@ -0,0 +1,55 @@
+import angular from 'angular';
+import angularMeteor from 'angular-meteor';
+import { Tasks } from '../../api/tasks.js';
+import template from './todosList.html';
+
+
+class TodosListCtrl {
+  constructor($scope) {
+    $scope.viewModel(this);
+
+    this.helpers({
+      tasks() {
+        return Tasks.find({}, {
+          sort: {
+            createdAt: -1
+          }
+        });
+      }
+    })
+  }
+
+
+  addTask(newTask) {
+    // Insert a task into the collection
+    Tasks.insert({
+      text: newTask,
+      createdAt: new Date
+    });
+
+    // Clear form
+    this.newTask = '';
+  }
+
+  setChecked(task) {
+    // Set the checked property to the opposite of its current value
+    Tasks.update(task._id, {
+      $set: {
+        checked: !task.checked
+      },
+    });
+  }
+
+  removeTask(task) {
+    Tasks.remove(task._id);
+  }
+
+}
+
+export default angular.module('todosList', [
+  angularMeteor
+])
+  .component('todosList', {
+    templateUrl: 'imports/components/todosList/todosList.html',
+    controller: ['$scope', TodosListCtrl]
+  });
